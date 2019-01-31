ca90ba1b775cfe833dba9955f68e1ac683756bee
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-31 00:51:25 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,11 @@
 class Backer
-  attr_accessor :name
+  attr_accessor :name, :backed_projects
+
   def initialization(name)
     @name = name
+    @backed_projects = []
+  end
+  def back_project(project)
+    @backed_projects << project
   end
 end
